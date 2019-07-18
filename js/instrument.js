86fddddb312309e43ed20e0c2f1b140394408b1f
SaakethC <saakethrchenna@gmail.com>
2019-07-17 21:10:22 -0500

    added basics
    add basics

---
+++ js/instrument.js
@@ -0,0 +1,13 @@
+
+
+document.getElementById("instrument").style.display = "none";
+
+function instumentsFunction (){
+    document.getElementById("instruments").style.display = "block";
+    document.getElementById("instrument").style.display = "none";
+}
+
+function piano () {
+    document.getElementById("instruments").style.display = "none";
+    document.getElementById("instrument").style.display = "block";
+};
